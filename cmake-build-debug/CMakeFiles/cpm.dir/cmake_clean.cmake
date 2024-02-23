9d95d9111bd0232e699354fe0c2eb8bf7dce4c36
João Fonseca <joao.fonseca@ua.pt>
2024-02-23 09:55:39 +0000

    add rest of config

---
+++ cmake-build-debug/CMakeFiles/cpm.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpm.dir/src/main.cpp.o"
+  "CMakeFiles/cpm.dir/src/main.cpp.o.d"
+  "cpm"
+  "cpm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
