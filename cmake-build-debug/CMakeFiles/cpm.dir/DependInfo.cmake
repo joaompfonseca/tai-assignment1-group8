831f070dc28dfc43b727a1adaff4a479b2d408b7
João Fonseca <joao.fonseca@ua.pt>
2024-03-25 16:22:50 +0000

    update readme

---
+++ cmake-build-debug/CMakeFiles/cpm.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/joaoo/OneDrive - Universidade de Aveiro/UC/TAI/Assignment/tai-assignment1-group8/src/copymodel/CopyModel.cpp" "CMakeFiles/cpm.dir/src/copymodel/CopyModel.cpp.o" "gcc" "CMakeFiles/cpm.dir/src/copymodel/CopyModel.cpp.o.d"
+  "/mnt/c/Users/joaoo/OneDrive - Universidade de Aveiro/UC/TAI/Assignment/tai-assignment1-group8/src/copymodel/CopyModelRunner.cpp" "CMakeFiles/cpm.dir/src/copymodel/CopyModelRunner.cpp.o" "gcc" "CMakeFiles/cpm.dir/src/copymodel/CopyModelRunner.cpp.o.d"
+  "/mnt/c/Users/joaoo/OneDrive - Universidade de Aveiro/UC/TAI/Assignment/tai-assignment1-group8/src/cpm.cpp" "CMakeFiles/cpm.dir/src/cpm.cpp.o" "gcc" "CMakeFiles/cpm.dir/src/cpm.cpp.o.d"
+  "/mnt/c/Users/joaoo/OneDrive - Universidade de Aveiro/UC/TAI/Assignment/tai-assignment1-group8/src/reader/FileReader.cpp" "CMakeFiles/cpm.dir/src/reader/FileReader.cpp.o" "gcc" "CMakeFiles/cpm.dir/src/reader/FileReader.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
